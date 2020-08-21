02d50b3d227c8aba4be00c50347ffcbcecf8228e
VasilisPoulos <cse42805@cs.uoi.gr>
2020-08-21 23:34:25 +0300

    Python script, new folder for nodebooks used for experimentation

---
+++ tang_module.py
@@ -0,0 +1,180 @@
+from difflib import SequenceMatcher
+from anytree import Node, RenderTree
+import numpy as np 
+import cv2 as cv
+import imutils
+
+# First block id + 1
+BLOCK_ID = 0 
+
+# List that holds block objects
+block_list = []
+
+class code_block:
+    def __init__(self, b_id, text, x, y, w, h):
+        self.b_id = b_id
+        self.text = text
+        self.x = x
+        self.y = y
+        self.width = w
+        self. height = h
+
+    def __str__(self):
+        return '{self.b_id}, "{self.text}" at ({self.x}, {self.y})'.format(self = self)
+
+
+def new_block(text, x, y, w, h):
+    global BLOCK_ID 
+    BLOCK_ID += 1
+    block_list.append(code_block(BLOCK_ID, text, x, y, w, h))
+    return block_list[BLOCK_ID - 1]
+
+
+def get_block_list():
+    return block_list
+
+
+def get_block_list_item(index):
+    return block_list[index]
+
+
+def get_underneath(my_block):
+    thr = 30
+    res_block =  None
+    for block in block_list:
+        # Skip the same block as my_block in the block_list or blocks that are 
+        # higher in the picture than my_block.
+        if block.b_id == my_block.b_id or block.y < my_block.y: 
+            # print('Skipped {}'.format(block.text))
+            continue
+        # Searching for a block that: 
+        # * is `not` more to the right or left than my block in the x axis 
+        #   + threshold
+        # * is `not` lower that my_block's height + threshold
+        if  my_block.x - thr < block.x < my_block.x + thr and             block.y < my_block.y + my_block.height + thr:
+            res_block = block
+    return res_block 
+
+
+# get_attached_to: returns the attached block to my_block if there is one
+# else None.
+def get_attached_to(my_block):
+    thr = 30
+    res_block = None
+    for block in block_list:
+        # Skip the same block as my_block in the block_list or blocks that are 
+        # more to the left of the picture than my_block.
+        if block.b_id == my_block.b_id or block.x < my_block.x:      
+            # print('Skipped {}'.format(block.text))
+            continue 
+        # Searching for a block that: 
+        # * is `not` higher or lower than my block in the y axis + threshold
+        # * is `not` more to the right that my_block's width + threshold
+        if  my_block.y - thr < block.y < my_block.y + thr and             block.x < my_block.x + my_block.width + thr :
+            res_block = block
+    return res_block 
+
+
+# get_indented_to: returns the indented block to my_block if there is one
+# else None.
+def get_indented_to(my_block):
+    res_block =  None
+    thr = 10
+    for block in block_list:
+        # Skip the same block as my_block in the block_list or blocks that are 
+        # higher than my_block.
+        if block.b_id == my_block.b_id or block.y < my_block.y: 
+            #print('Skipped {}'.format(block.text))
+            continue
+        # Searching for a block that: 
+        # * is within the range of my_block's width + threshold
+        # * is below my_block
+        if  my_block.x < block.x < my_block.x + my_block.width and             block.y < my_block.y + my_block.height + thr:
+            res_block = block
+
+    return res_block
+
+
+# similar(): 
+# returns: a precentage of string a, b similarity
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def print_AST(root):
+    # Print resulting AST
+    for pre, _, node in RenderTree(root):
+        print("%s%s" % (pre, node.name))
+
+# Perspective transform functions @pyimagesearch.com
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv.getPerspectiveTransform(rect, dst)
+    warped = cv.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    return warped
+
+
+def find_points(image):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (3, 3), 0)
+    edged = cv.Canny(gray, 75, 200)
+
+    cnts = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:5]
+    for c in cnts:
+        peri = cv.arcLength(c, True)
+        approx = cv.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+
+    return screenCnt
---
+++ tangibles.py
@@ -0,0 +1,209 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import cv2 as cv
+import pytesseract
+import os
+import imutils
+import argparse
+import tang_module as tg
+from anytree import Node, RenderTree
+from PIL import Image
+
+
+DILATION_WINDOW_SIZE = 3
+DILATION_ITERATIONS = 5
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True,
+                help = "Path to tangible program image")
+ap.add_argument('-d', '--debug', action='store_true',
+                help = "Activate debug output")
+ap.add_argument('-a', '--all', action='store_true',
+                help = "Activate debug output")
+args = vars(ap.parse_args())
+
+# Open image
+image = cv.imread(args['image'])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
+
+# Perspective transformation
+screenCnt = tg.find_points(image)
+image = tg.four_point_transform(orig, screenCnt.reshape(4,2)*ratio)
+
+# Original image to HSV
+hsv_A = cv.cvtColor(image, cv.COLOR_RGB2HSV)
+
+# Split each chanel
+h,s,v = cv.split(hsv_A)
+
+# Original image to grayscale
+gray_A = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# Threshold saturation chanel
+ret3, th_saturation = cv.threshold(s, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+# Dilating thresholded image to remove letters
+kernel = cv.getStructuringElement(cv.MORPH_RECT,\
+    (DILATION_WINDOW_SIZE,DILATION_WINDOW_SIZE))
+th_saturation = cv.dilate(th_saturation, kernel \
+                        ,iterations = DILATION_ITERATIONS)
+
+# Erosion 
+kernel = np.ones((5,5),np.uint8)
+th_saturation = cv.erode(th_saturation,kernel,iterations = 5)
+
+# Using connected components (CC) method to label each block
+num_labels, labels_im = cv.connectedComponents(th_saturation)
+
+# Kernel that will be used in closing morphological trasformation
+closing_kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
+
+# Used for filtering components
+low_filter = .0002*np.prod(image.shape) 
+
+if args['debug']:
+        plt.title('Masking preperation')
+        plt.imshow(th_saturation, cmap='gray')
+        plt.show()
+
+block_counter = 0
+for num in range(1, num_labels):
+
+    # Extracting mask from (CC) result
+    label = labels_im == num
+    block_mask = np.copy(th_saturation)
+    block_mask[label == False] = 0
+    block_mask[label == True] = 255
+
+    plt.imshow(block_mask)
+    plt.show()
+    # Find coordinates (x, y) and hight - width of the block (h, w) based on 
+    # the blocks mask
+    _, contours, _ = cv.findContours(block_mask, cv.RETR_TREE, \
+            cv.CHAIN_APPROX_SIMPLE)
+    x,y,w,h = cv.boundingRect(contours[0])
+
+    
+    # Masking to extract image feature from image
+    feature = cv.bitwise_and(gray_A, gray_A, mask=block_mask)  
+    
+    if cv.countNonZero(feature) < low_filter:
+        continue
+
+    # Cropping each block to help with binarization later
+    # Using a temporary cropping solution to crop control blocks 
+    # TODO: find a better way to crop all blocks 
+    # (maybe use image_to_data() from Tesseract)
+    crop_v = 18
+    if h > 150:
+        feature = feature[y+crop_v:y+h-crop_v-80, x+crop_v: x+w-crop_v-10]
+    else:
+        feature = feature[y+crop_v:y+h-crop_v, x+crop_v: x+w-crop_v]
+    
+    # Upscale feature image
+    multiplier = 2
+    feature = cv.resize(feature, dsize=(feature.shape[1]*multiplier, \
+        feature.shape[0]*multiplier), interpolation=cv.INTER_CUBIC)
+
+    # Binarize and invert feature
+    ret3, th_feature = cv.threshold(feature, 0, 255, \
+        cv.THRESH_BINARY+cv.THRESH_OTSU)
+    closing = cv.morphologyEx(th_feature, cv.MORPH_CLOSE, closing_kernel)
+    inv_feature = np.invert(closing)
+    
+    # Tesseract 
+    config = '--psm 7'
+    text_in_block = pytesseract.image_to_string(inv_feature, \
+            lang='eng', config=config)
+
+    # Save block information
+    nb = tg.new_block( ' '.join(text_in_block.split()), x, y, w, h)
+    
+    block_counter += 1
+    # Print pre-process/ Tesseract results
+    if args['debug'] and args['all']:
+        plt.title('Block\'s id: {} at ({}, {})\n Tesseract read: {}'\
+            .format(nb.b_id, nb.x, nb.y, ' '.join(text_in_block.split())))
+        plt.imshow(th_feature, cmap='gray')
+        plt.show()
+
+print('Found {} blocks in image'.format(block_counter))
+
+## Main logic for generating AST and result ##
+
+# Root of the AST
+root = Node('tangible program')
+next_block = None
+ # current identation level
+nesting_level = root   
+# init first node
+start = Node(tg.get_block_list_item(0).text, parent=nesting_level) 
+previous_node = start
+  # save all nesting levels so i can go back
+nesting_levels = []   
+nesting_levels.append(nesting_level)
+ # index of nesting_level list
+index = 0              
+
+# Assuming all blocks in the list are sorted
+for i in range(1, len(tg.block_list)):
+
+    ## "Special blocks" ##
+    if tg.similar(tg.block_list[i].text, 'b_tab') > 0.8: 
+        # TODO: This can be done without relying on this blocks text
+        # print('{} is {} similar to b_tab'.format(block_list[i].text,\
+        #   similar(block_list[i].text, 'b_tab')))
+        if index != 0:
+            index -= 1
+        continue
+
+    if tg.similar(tg.block_list[i].text, 'repeat indefinitely do') > 0.7: 
+        # TODO: or any other control block
+        new_node = Node(tg.block_list[i].text, parent=nesting_levels[index])
+        previous_node = new_node
+        nesting_level = new_node
+        nesting_levels.append(nesting_level)
+        index += 1
+        continue
+    ##  #  #  #  #  #  ##
+
+    res = tg.get_attached_to(tg.block_list[i-1])
+    if res != None and res.b_id == tg.block_list[i].b_id:
+        # the block is to the right of the previous block
+        # attach node to previous Node
+        new_node = Node(res.text, parent=previous_node)
+        previous_node = new_node
+        continue
+
+    res = tg.get_indented_to(tg.block_list[i-1])
+    if res != None and res.b_id == tg.block_list[i].b_id:
+        # print('{} is indented to {}'.format(res.text,block_list[i-1].text))
+        # the block is indented to the previous block
+        new_node = Node(res.text, parent=previous_node)
+        nesting_level = new_node
+        nesting_levels.append(nesting_level)
+        index += 1
+        previous_node = new_node
+        continue
+
+    res = tg.get_underneath(tg.block_list[i-1])
+    if res != None and res.b_id == tg.block_list[i].b_id:
+        # the block is underneath the previous block
+
+        if tg.similar(tg.block_list[i-1].text, 'Start') > 0.7:
+            new_node = Node(tg.block_list[i].text, parent=previous_node)
+        else:
+            new_node = Node(tg.block_list[i].text, parent=nesting_levels[index])
+        previous_node = new_node
+        continue
+
+    if res == None:
+        # Nothing of the previous, add it to the current nest level
+        new_node = Node(tg.block_list[i].text, parent=nesting_levels[index])
+        previous_node = new_node
+        continue
+   
+tg.print_AST(root)
