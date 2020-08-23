a09912b50f5aea386034c6040154d461751a97d5
VasilisPoulos <cse42805@cs.uoi.gr>
2020-08-23 10:36:45 +0300

    Small adjustments

--- tangibles.py
+++ tangibles.py
@@ -9,9 +9,11 @@ import tang_module as tg
 from anytree import Node, RenderTree
 from PIL import Image
 
-
+# Pre-processing constants
 DILATION_WINDOW_SIZE = 3
 DILATION_ITERATIONS = 5
+EROSION_WINDOW_SIZE = 3
+EROSION_ITERATIONS = 3
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -52,8 +54,8 @@ th_saturation = cv.dilate(th_saturation, kernel \
                         ,iterations = DILATION_ITERATIONS)
 
 # Erosion 
-kernel = np.ones((5,5),np.uint8)
-th_saturation = cv.erode(th_saturation,kernel,iterations = 5)
+kernel = np.ones((EROSION_WINDOW_SIZE,EROSION_WINDOW_SIZE),np.uint8)
+th_saturation = cv.erode(th_saturation,kernel,iterations = EROSION_ITERATIONS)
 
 # Using connected components (CC) method to label each block
 num_labels, labels_im = cv.connectedComponents(th_saturation)
@@ -65,9 +67,14 @@ closing_kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
 low_filter = .0002*np.prod(image.shape) 
 
 if args['debug']:
-        plt.title('Masking preperation')
-        plt.imshow(th_saturation, cmap='gray')
-        plt.show()
+    fig = plt.figure(figsize=(20,10))
+    plt.subplot(1, 3, 1)
+    plt.imshow(cv.cvtColor(orig, cv.COLOR_BGR2RGB))
+    plt.subplot(1, 3, 2)
+    plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))
+    plt.subplot(1, 3, 3)
+    plt.imshow(th_saturation)
+    plt.show()
 
 block_counter = 0
 for num in range(1, num_labels):
@@ -78,8 +85,7 @@ for num in range(1, num_labels):
     block_mask[label == False] = 0
     block_mask[label == True] = 255
 
-    plt.imshow(block_mask)
-    plt.show()
+
     # Find coordinates (x, y) and hight - width of the block (h, w) based on 
     # the blocks mask
     _, contours, _ = cv.findContours(block_mask, cv.RETR_TREE, \
@@ -152,7 +158,7 @@ index = 0
 for i in range(1, len(tg.block_list)):
 
     ## "Special blocks" ##
-    if tg.similar(tg.block_list[i].text, 'b_tab') > 0.8: 
+    if tg.similar(tg.block_list[i].text, 'b_tab') > 0.7: 
         # TODO: This can be done without relying on this blocks text
         # print('{} is {} similar to b_tab'.format(block_list[i].text,\
         #   similar(block_list[i].text, 'b_tab')))
@@ -193,10 +199,10 @@ for i in range(1, len(tg.block_list)):
     if res != None and res.b_id == tg.block_list[i].b_id:
         # the block is underneath the previous block
 
-        if tg.similar(tg.block_list[i-1].text, 'Start') > 0.7:
-            new_node = Node(tg.block_list[i].text, parent=previous_node)
-        else:
-            new_node = Node(tg.block_list[i].text, parent=nesting_levels[index])
+        #if tg.similar(tg.block_list[i-1].text, 'Start') > 0.7:
+        #    new_node = Node(tg.block_list[i].text, parent=previous_node)
+        #else:
+        new_node = Node(tg.block_list[i].text, parent=nesting_levels[index])
         previous_node = new_node
         continue
 
