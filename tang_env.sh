b5291d48bd449a3ace3a4e7feb3637b1ccf9931c
VasilisPoulos <cse42805@cs.uoi.gr>
2020-08-25 21:14:15 +0300

    bash script from automated python env setup

---
+++ tang_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo 'Give a name for the tangibles python environment'
+read env_name
+python -m venv $env_name
+source $env_name/bin/activate
+
+pip install --upgrade pip
+pip install numpy
+pip install matplotlib
+pip install opencv-python==3.4.11.41
+pip install pytesseract
+pip install imutils
+pip install anytree
+sudo apt-get install tesseract-ocr
